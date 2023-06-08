f9dfeafd761dc371541c8854de83f80fd4a03043
GanXing <starcloud@StarclouddeMacBook-Pro.local>
2023-06-08 10:49:43 +0800

    fix bug delete unuse code删除无用代码

--- src/ui-component/Logo.tsx
+++ src/ui-component/Logo.tsx
@@ -1,19 +1,4 @@
-// material-ui
-import { useTheme } from '@mui/material/styles';
-
-/**
- * if you want to use image instead of <svg> uncomment following.
- *
- * import logoDark from 'assets/images/logo-dark.svg';
- * import logo from 'assets/images/logo.svg';
- *
- */
-
-// ==============================|| LOGO SVG ||============================== //
-
 const Logo = () => {
-    const theme = useTheme();
-
     return (
         /**
          * if you want to use image instead of svg uncomment following, and comment out <svg> element.

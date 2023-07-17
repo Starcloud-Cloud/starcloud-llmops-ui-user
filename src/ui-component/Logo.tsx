09c944def566ed4982db289d63679dfaf165cfd9
Walker Peng <939212596@qq.com>
2023-07-17 21:10:33 +0800

    fix: 修改logo des大小

--- src/ui-component/Logo.tsx
+++ src/ui-component/Logo.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography, useScrollTrigger, useTheme } from '@mui/material';
-import { cloneElement, ReactElement } from 'react';
+import { ReactElement, cloneElement } from 'react';
 interface ElevationScrollProps {
     children: ReactElement;
     window?: Window | Node;
@@ -237,8 +237,8 @@ const Logo = ({ ...others }) => {
                     <Typography
                         variant="body1"
                         sx={{
-                            fontSize: '1em',
-                            lineHeight: '1em',
+                            fontSize: '12px',
+                            lineHeight: '14px',
                             textAlign: 'center',
                             overflow: 'hidden',
                             textOverflow: 'ellipsis',

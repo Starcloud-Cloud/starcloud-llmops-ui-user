de32d520e48d38bbe42e55fb92205963703aaf00
Walker Peng <939212596@qq.com>
2023-08-31 16:25:18 +0800

    update

--- src/views/template/myChat/createChat/components/Skill.tsx
+++ src/views/template/myChat/createChat/components/Skill.tsx
@@ -24,6 +24,8 @@ import { gridSpacing } from 'store/constant';
 import { TabsProps } from 'types';
 import MainCard from 'ui-component/cards/MainCard';
 import { IChatInfo } from '../index';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 function TabPanel({ children, value, index, ...other }: TabsProps) {
     return (
@@ -159,34 +161,18 @@ const QAModal = ({ open, handleClose }: { open: boolean; handleClose: () => void
     );
 };
 
-const DocumentModal = ({ open, handleClose }: { open: boolean; handleClose: () => void }) => {
-    const theme = useTheme();
-    const [valueLabel, setValueLabel] = useState('checked');
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-    };
-
-    const { Dragger } = Upload;
-
-    const props: UploadProps = {
-        name: 'file',
-        multiple: true,
-        action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-        onChange(info) {
-            const { status } = info.file;
-            if (status !== 'uploading') {
-                console.log(info.file, info.fileList);
-            }
-            if (status === 'done') {
-            } else if (status === 'error') {
-            }
+const ApiModal = ({ open, handleClose }: { open: boolean; handleClose: () => void }) => {
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            context: ''
         },
-        onDrop(e) {
-            console.log('Dropped files', e.dataTransfer.files);
-        }
-    };
+        validationSchema: yup.object({
+            title: yup.string().required('标题是必填的'),
+            context: yup.string().max(150000, '文本过长、请减少到150000字以内').required('内容是必填的')
+        }),
+        onSubmit: (values) => {}
+    });
 
     return (
         <Modal open={open} onClose={handleClose} aria-labelledby="modal-title" aria-describedby="modal-description">
@@ -207,74 +193,20 @@ const DocumentModal = ({ open, handleClose }: { open: boolean; handleClose: () =
                 }
             >
                 <CardContent>
-                    <>
-                        <Tabs
-                            value={value}
-                            variant="scrollable"
-                            onChange={handleChange}
-                            sx={{
-                                mb: 3,
-                                '& a': {
-                                    minHeight: 'auto',
-                                    minWidth: 10,
-                                    py: 1.5,
-                                    px: 1,
-                                    mr: 2.2,
-                                    color: theme.palette.grey[600],
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    alignItems: 'center',
-                                    justifyContent: 'center'
-                                },
-                                '& a.Mui-selected': {
-                                    color: theme.palette.primary.main
-                                },
-                                '& a > svg': {
-                                    mb: '0px !important',
-                                    mr: 1.1
-                                }
-                            }}
-                        >
-                            <Tab component={Link} label="上传文档" {...a11yProps(0)} />
-                            <Tab component={Link} label="文本输入" {...a11yProps(1)} />
-                            <Tab component={Link} label="网页抓取" {...a11yProps(2)} />
-                        </Tabs>
-                        <TabPanel value={value} index={0}>
-                            <div className="text-sm text-[#9da3af]">
-                                格式支持 .pdf .docx .txt .pptx .epub .md .csv，请确保内容可复制，每个30MB以内，单次最多上传20个。
-                                文档中的表格和图片暂时无法学习。
-                            </div>
-                            <div className="mt-3">
-                                <Dragger {...props}>
-                                    <p className="ant-upload-drag-icon">
-                                        <InboxOutlined rev={undefined} />
-                                    </p>
-                                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                                    <p className="ant-upload-hint">
-                                        Support for a single or bulk upload. Strictly prohibited from uploading company data or other banned
-                                        files.
-                                    </p>
-                                </Dragger>
-                            </div>
-                        </TabPanel>
-                        <TabPanel value={value} index={1}>
-                            <TextField label={'标题'} fullWidth />
-                            <TextField label={'内容'} className={'mt-3'} fullWidth multiline minRows={6} />
-                        </TabPanel>
-                        <TabPanel value={value} index={2}>
-                            <div className="text-sm text-[#9da3af]">
-                                请避免非法抓取他人网站的侵权行为，保证链接可公开访问，且网站内容可复制
-                            </div>
-                            <TextField
-                                label={'网页地址'}
-                                className={'mt-3'}
-                                fullWidth
-                                multiline
-                                minRows={6}
-                                placeholder="输入要爬取的网页地址，使用英文,分隔"
-                            />
-                        </TabPanel>
-                    </>
+                    <form onSubmit={formik.handleSubmit}>
+                        <TextField
+                            label={'标题'}
+                            fullWidth
+                            id="title"
+                            name="title"
+                            color="secondary"
+                            InputLabelProps={{ shrink: true }}
+                            value={formik.values.title}
+                            onChange={formik.handleChange}
+                            error={formik.touched.title && Boolean(formik.errors.title)}
+                            helperText={formik.touched.title && formik.errors.title}
+                        />
+                    </form>
                 </CardContent>
                 <Divider />
                 <CardActions>
@@ -293,7 +225,7 @@ export const Skill = ({ chatBotInfo, setChatBotInfo }: { chatBotInfo: IChatInfo;
     const theme = useTheme();
     const [anchorEl, setAnchorEl] = useState<Element | ((element: Element) => Element) | null | undefined>(null);
     const [qaVisible, setQaVisible] = useState(false);
-    const [documentVisible, setDocumentVisible] = useState(false);
+    const [apiVisible, setApiVisible] = useState(false);
 
     const [isValid, setIsValid] = useState(true);
     const [websiteCount, setWebsiteCount] = useState(0);
@@ -333,7 +265,7 @@ export const Skill = ({ chatBotInfo, setChatBotInfo }: { chatBotInfo: IChatInfo;
                     学习工作流程
                 </span>
                 <div className="text-sm text-[#9da3af] ml-3">
-                    让你的机器人可直接执行定制的AI应用，实现更复杂和深度的内容创作和工作内容。{' '}
+                    让你的机器人可直接执行定制的AI应用，实现更复杂和深度的内容创作和工作内容。
                 </div>
                 <div className={'mt-3'}>
                     <MainCard>
@@ -388,30 +320,7 @@ export const Skill = ({ chatBotInfo, setChatBotInfo }: { chatBotInfo: IChatInfo;
                                 direction="row"
                                 spacing={gridSpacing}
                                 className={'h-[220px] flex justify-center items-center flex-col cursor-pointer'}
-                            >
-                                <Popover
-                                    content={
-                                        <div className="flex justify-start items-center flex-col">
-                                            <div className="text-sm text-center w-[330px]">
-                                                <div>功能正在封闭测试中。</div>
-                                                <div>可联系我们产品顾问进一步了解，</div>
-                                                <div>并获得提前免费使用的权利。</div>
-                                            </div>
-                                            <img className="w-40" src={workWechatPay} alt="" />
-                                        </div>
-                                    }
-                                    trigger="hover"
-                                >
-                                    <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="128" height="128">
-                                        <path
-                                            d="M880.64 358.4h-819.2v423.936c0 34.816 26.624 61.44 61.44 61.44h491.52c12.288 0 20.48 8.192 20.48 20.48s-8.192 20.48-20.48 20.48h-491.52c-57.344 0-102.4-45.056-102.4-102.4v-552.96c0-57.344 45.056-102.4 102.4-102.4h696.32c57.344 0 102.4 45.056 102.4 102.4v176.128c0 12.288-8.192 20.48-20.48 20.48s-20.48-8.192-20.48-20.48v-47.104z m0-40.96v-88.064c0-34.816-26.624-61.44-61.44-61.44h-696.32c-34.816 0-61.44 26.624-61.44 61.44v88.064h819.2z m-204.8-51.2c-12.288 0-20.48-8.192-20.48-20.48s8.192-20.48 20.48-20.48 20.48 8.192 20.48 20.48-8.192 20.48-20.48 20.48z m61.44 0c-12.288 0-20.48-8.192-20.48-20.48s8.192-20.48 20.48-20.48 20.48 8.192 20.48 20.48-8.192 20.48-20.48 20.48z m61.44 0c-12.288 0-20.48-8.192-20.48-20.48s8.192-20.48 20.48-20.48 20.48 8.192 20.48 20.48-8.192 20.48-20.48 20.48z m-448.512 241.664c6.144-10.24 18.432-12.288 28.672-8.192 10.24 6.144 12.288 18.432 8.192 28.672l-102.4 178.176c-6.144 10.24-18.432 12.288-28.672 8.192s-12.288-18.432-8.192-28.672l102.4-178.176z m-126.976 6.144l-55.296 90.112 55.296 94.208c6.144 10.24 2.048 22.528-8.192 28.672-10.24 6.144-22.528 2.048-28.672-8.192l-67.584-114.688 67.584-110.592c6.144-10.24 18.432-12.288 28.672-6.144 10.24 4.096 12.288 16.384 8.192 26.624z m188.416 184.32l55.296-94.208-55.296-90.112c-6.144-10.24-2.048-22.528 6.144-28.672 10.24-6.144 22.528-2.048 28.672 6.144l67.584 110.592-67.584 114.688c-6.144 10.24-18.432 12.288-28.672 8.192-8.192-4.096-10.24-18.432-6.144-26.624z m577.536-122.88l4.096 10.24-40.96 51.2c-8.192 10.24-8.192 26.624 0 36.864l38.912 47.104-4.096 10.24c-8.192 26.624-22.528 51.2-38.912 71.68l-8.192 10.24-61.44-10.24c-12.288-2.048-26.624 6.144-30.72 18.432l-20.48 61.44-10.24 2.048c-32.768 8.192-69.632 8.192-102.4 0l-12.288-2.048-20.48-61.44c-4.096-12.288-18.432-20.48-30.72-18.432l-63.488 10.24-8.192-8.192c-8.192-10.24-16.384-20.48-22.528-32.768-8.192-12.288-14.336-26.624-18.432-40.96l-4.096-10.24 40.96-49.152c8.192-10.24 8.192-26.624 0-36.864l-40.96-49.152 4.096-10.24c10.24-26.624 22.528-51.2 40.96-73.728l8.192-8.192 61.44 10.24c12.288 2.048 26.624-6.144 30.72-18.432l22.528-61.44 10.24-2.048c32.768-6.144 67.584-6.144 100.352 0l12.288 2.048 20.48 59.392c4.096 12.288 18.432 20.48 30.72 20.48l63.488-8.192 8.192 8.192c8.192 10.24 16.384 20.48 22.528 32.768 8.192 12.288 14.336 24.576 18.432 38.912z m-53.248-20.48l-12.288-18.432-38.912 4.096c-32.768 4.096-65.536-16.384-75.776-47.104l-12.288-36.864c-20.48-4.096-40.96-4.096-61.44 0l-14.336 38.912c-10.24 30.72-45.056 51.2-75.776 45.056l-36.864-6.144c-10.24 12.288-16.384 26.624-22.528 40.96l26.624 30.72c20.48 24.576 20.48 63.488 0 90.112l-26.624 30.72c4.096 8.192 6.144 16.384 12.288 24.576 4.096 6.144 6.144 12.288 10.24 16.384l40.96-6.144c32.768-4.096 65.536 16.384 75.776 47.104l12.288 38.912c20.48 4.096 40.96 4.096 61.44 0l14.336-40.96c10.24-30.72 45.056-51.2 75.776-45.056l36.864 6.144c8.192-12.288 16.384-26.624 22.528-40.96l-24.576-28.672c-20.48-24.576-20.48-63.488-2.048-88.064l26.624-32.768c-4.096-6.144-8.192-14.336-12.288-22.528z m-169.984 202.752c-57.344 0-102.4-45.056-102.4-102.4s45.056-102.4 102.4-102.4 102.4 45.056 102.4 102.4c0 55.296-47.104 102.4-102.4 102.4z m0-40.96c34.816 0 61.44-26.624 61.44-61.44s-26.624-61.44-61.44-61.44-61.44 26.624-61.44 61.44 26.624 61.44 61.44 61.44z"
-                                            fill="#515151"
-                                            p-id="6181"
-                                        ></path>
-                                    </svg>
-                                </Popover>
-                                <div className="text-base">即将推出</div>
-                            </Grid>
+                            ></Grid>
                         </MainCard>
                     </div>
                 </div>
@@ -475,7 +384,7 @@ export const Skill = ({ chatBotInfo, setChatBotInfo }: { chatBotInfo: IChatInfo;
                 )}
             </div>
             <QAModal open={qaVisible} handleClose={() => setQaVisible(false)} />
-            <DocumentModal open={documentVisible} handleClose={() => setDocumentVisible(false)} />
+            <ApiModal open={apiVisible} handleClose={() => setApiVisible(false)} />
         </div>
     );
 };

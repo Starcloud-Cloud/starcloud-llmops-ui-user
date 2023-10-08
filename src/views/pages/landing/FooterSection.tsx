031be6763ecd4ccbb350fe0952e755bbca589f2b
Walker Peng <939212596@qq.com>
2023-10-08 18:01:54 +0800

    feat: update

--- src/views/pages/landing/FooterSection.tsx
+++ src/views/pages/landing/FooterSection.tsx
@@ -149,6 +149,9 @@ const FooterSection = () => {
                     <a target="_blank" className="text-[#2196f3] ml-2" href="https://www.aiwht.com">
                         AI万花筒
                     </a>
+                    <a target="_blank" className="text-[#2196f3] ml-2" href="https://www.aitop100.cn">
+                        AI Top100
+                    </a>
                 </div>
             </div>
             <Box sx={{ bgcolor: 'dark.dark', py: { xs: 3, sm: 1.5 } }}>

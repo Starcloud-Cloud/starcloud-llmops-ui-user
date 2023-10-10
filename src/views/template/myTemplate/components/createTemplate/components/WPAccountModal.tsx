7570de108e6ed6ee5d6c17016b404f0aaf193067
shenqichao <2310776744@qq.com>
2023-10-10 15:57:33 +0800

    微信公众号

--- src/views/template/myTemplate/components/createTemplate/components/WPAccountModal.tsx
+++ src/views/template/myTemplate/components/createTemplate/components/WPAccountModal.tsx
@@ -1,6 +1,6 @@
 // material-ui
 import CloseIcon from '@mui/icons-material/Close';
-import { CardContent, IconButton, Button, Modal, Tabs, Tab, Box, Typography, Divider } from '@mui/material';
+import { CardContent, IconButton, Button, Modal, Tabs, Tab, Box, Typography, Divider, Link, TextField } from '@mui/material';
 import { useState } from 'react';
 import MainCard from 'ui-component/cards/MainCard';
 interface TabPanelProps {
@@ -142,12 +142,84 @@ export default function WechatModal({ open, setOpen }: { open: boolean; setOpen:
                     >
                         <Tab label="添加菜单" {...a11yProps(0)} />
                         <Tab label="自动回复" {...a11yProps(1)} />
+                        <Tab label="消息调用" {...a11yProps(1)} />
                     </Tabs>
                     <CustomTabPanel value={value} index={0}>
                         <WeSetting name="添加菜单" />
                     </CustomTabPanel>
                     <CustomTabPanel value={value} index={1}>
                         <WeSetting name="自动回复" />
+                    </CustomTabPanel>
+                    <CustomTabPanel value={value} index={2}>
+                        <Link sx={{ mb: 2 }} color="secondary">
+                            查看示例图
+                        </Link>
+                        <div>
+                            <div>
+                                进入微信 <span className="text-[#2AC74A] mt-[8px]">「公众号后台」</span> 复制开发信息
+                            </div>
+                            <div className="text-[12px] text-[#697586] mt-[8px]">{`路径：设置与开发 > 基本配置，启用开发者密码后，复制并填写在下方`}</div>
+                            <div className="text-[12px] text-[#EA0000] mt-[8px]">未认证公众号暂不支持</div>
+                            <div className="text-[12px] text-[#EA0000] mt-[8px]">启用后，请在微信公众号后台复制IP白名单</div>
+                            <TextField
+                                sx={{ mt: 2, mb: 1 }}
+                                label="开发者ID"
+                                color="secondary"
+                                helperText=" "
+                                fullWidth
+                                required
+                                name="id"
+                                InputLabelProps={{ shrink: true }}
+                            />
+                            <TextField
+                                label="开发者密码"
+                                color="secondary"
+                                helperText=" "
+                                fullWidth
+                                required
+                                name="password"
+                                InputLabelProps={{ shrink: true }}
+                            />
+                            <div className="flex justify-end">
+                                <Button sx={{ width: '200px' }} color="secondary" variant="contained">
+                                    提交
+                                </Button>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-[500]">服务器配置</div>
+                            <div className="text-[#697586] my-[8px]">公众号后台启用后，请在微信公众号后台复制IP白名单</div>
+                            <div className="text-[#697586]">复制下方信息，在后台修改配置时粘贴</div>
+                            <div className="flex justify-between mt-[8px]">
+                                <div>服务器地址（URL）：</div>
+                                <Button size="small" color="secondary">
+                                    复制
+                                </Button>
+                            </div>
+                            <div className="flex justify-between">
+                                <div>令牌（Token）：</div>
+                                <Button size="small" color="secondary">
+                                    复制
+                                </Button>
+                            </div>
+                            <div className="flex justify-between">
+                                <div>消息加解密密钥（EncodingAESKey）：</div>
+                                <Button size="small" color="secondary">
+                                    复制
+                                </Button>
+                            </div>
+                            <div className="flex justify-between">
+                                <div>ip白名单：</div>
+                                <Button size="small" color="secondary">
+                                    复制
+                                </Button>
+                            </div>
+                            <div className="flex justify-between">
+                                <div>消息加解密方式</div>
+                                <div>明文模式</div>
+                            </div>
+                            <Button variant="outlined">重新部署</Button>
+                        </div>
                     </CustomTabPanel>
                 </CardContent>
             </MainCard>

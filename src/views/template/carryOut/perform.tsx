a3636e1f205264cdccddf080430e6051a2c85416
shenqichao <2310776744@qq.com>
2023-10-16 14:42:11 +0800

    修改

--- src/views/template/carryOut/perform.tsx
+++ src/views/template/carryOut/perform.tsx
@@ -97,8 +97,8 @@ function Perform({
     useEffect(() => {
         if (mdRef.current && mdRef.current.length > 0) {
             config?.steps?.map((item: any, index: number) => {
-                if (item.flowStep.response.style !== 'IMAGE') {
-                    mdRef.current[index].scrollTop = mdRef.current[index].scrollHeight;
+                if (item.flowStep.response.style !== 'IMAGE' && mdRef.current[index]) {
+                    mdRef.current[index].scrollTop = mdRef.current[index]?.scrollHeight;
                 }
             });
         }

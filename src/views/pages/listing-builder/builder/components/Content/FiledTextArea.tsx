194daed36c7e950d4093958d573c72c0698982c9
Walker Peng <939212596@qq.com>
2023-10-19 15:32:54 +0800

    feat: update

--- src/views/pages/listing-builder/builder/components/Content/FiledTextArea.tsx
+++ src/views/pages/listing-builder/builder/components/Content/FiledTextArea.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
 const FiledTextArea = ({ rows, value, handleInputChange, placeholder, index }: any) => {
+    const h_input = ['iphone', 'iphone pro'];
+    h_input.sort((a, b) => b.length - a.length);
+    const r = `(${h_input.join('|')})`;
+    const pattern = new RegExp(r);
+    const resultArray = value?.split(pattern);
+
     return (
-        <textarea
-            rows={rows}
-            placeholder={placeholder}
-            spellCheck="false"
-            value={value}
-            onChange={(e) => handleInputChange(e, index)}
-            className="border-[#e6e8ec] border-l-0 border-r-0 text-sm"
-        />
+        <div className="relative w-full">
+            <div className="break-words w-full h-full absolute p-[2px]">
+                {resultArray?.map((item: any) => (
+                    <pre
+                        className={`${
+                            h_input.includes(item)
+                                ? 'text-transparent inline whitespace-pre-wrap text-sm font-[monospace] bg-red-400'
+                                : 'text-transparent inline whitespace-pre-wrap text-sm font-[monospace]'
+                        }`}
+                    >
+                        {item}
+                    </pre>
+                ))}
+            </div>
+            <textarea
+                rows={rows}
+                placeholder={placeholder}
+                spellCheck="false"
+                value={value}
+                onChange={(e) => handleInputChange(e, index)}
+                style={{ background: 'none' }}
+                className="border-[#e6e8ec] border-l-0 border-r-0 text-sm relative z-10 w-full"
+            />
+        </div>
     );
 };
 

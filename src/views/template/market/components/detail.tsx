ea9f76d507a8d946b03d05518b58526169112ac7
shenqichao <2310776744@qq.com>
2023-10-25 18:00:39 +0800

    图片

--- src/views/template/market/components/detail.tsx
+++ src/views/template/market/components/detail.tsx
@@ -346,7 +346,7 @@ function Deatail() {
                             e.stopPropagation();
                         }}
                     >
-                        <GradeOutlined color="secondary" />
+                        <GradeOutlined sx={{ color: '#0003' }} />
                     </div>
                 ) : (
                     <div

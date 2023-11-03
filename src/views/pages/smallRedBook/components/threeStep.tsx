7509c1c4d4a6f987e1a29439dad14d3642641856
Walker Peng <939212596@qq.com>
2023-11-03 18:21:43 +0800

    feat: update

--- src/views/pages/smallRedBook/components/threeStep.tsx
+++ src/views/pages/smallRedBook/components/threeStep.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Card, CollapseProps, Divider, Space, Button } from 'antd';
+import { Avatar, Card, CollapseProps, Divider, Space, Button, Spin } from 'antd';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -109,73 +109,77 @@ export const ThreeStep = ({ data }: { data: any }) => {
             <Space direction="vertical" size={16} className="w-full">
                 <Card title="小红书生成" extra={<Button onClick={handleCreate}>生成</Button>}>
                     <div className="w-full grid grid-cols-3 h-[60vh]">
-                        <div className="col-span-2 relative">
-                            {images.length > 0 && (
-                                <>
-                                    <div className="flex justify-between absolute top-[46%] w-full z-10">
-                                        <Button
-                                            icon={<KeyboardBackspaceIcon />}
-                                            shape="circle"
-                                            onClick={() => {
-                                                console.log(swiperRef, 'swiperRef');
-                                                swiperRef?.slidePrev();
-                                            }}
-                                        />
-                                        <Button
-                                            style={{ marginLeft: '10px' }}
-                                            icon={<ArrowForwardIcon />}
-                                            shape="circle"
-                                            onClick={() => {
-                                                swiperRef?.slideNext();
-                                            }}
-                                        />
-                                    </div>
-                                    <div className="mt-[20px]">
-                                        {images.length > 0 && (
-                                            <Swiper
-                                                onSwiper={(swiper) => setSwiperRef(swiper)}
-                                                slidesPerView={1}
-                                                spaceBetween={30}
-                                                centeredSlides={false}
-                                                loop
-                                                modules={[]}
-                                                className="mySwiper"
-                                                autoplay={{
-                                                    delay: 2500,
-                                                    disableOnInteraction: false
+                        <Spin tip="Loading..." spinning={imgLoading}>
+                            <div className="col-span-2 relative">
+                                {images.length > 0 && (
+                                    <>
+                                        <div className="flex justify-between absolute top-[46%] w-full z-10">
+                                            <Button
+                                                icon={<KeyboardBackspaceIcon />}
+                                                shape="circle"
+                                                onClick={() => {
+                                                    console.log(swiperRef, 'swiperRef');
+                                                    swiperRef?.slidePrev();
                                                 }}
+                                            />
+                                            <Button
+                                                style={{ marginLeft: '10px' }}
+                                                icon={<ArrowForwardIcon />}
+                                                shape="circle"
+                                                onClick={() => {
+                                                    swiperRef?.slideNext();
+                                                }}
+                                            />
+                                        </div>
+                                        <div className="mt-[20px]">
+                                            {images.length > 0 && (
+                                                <Swiper
+                                                    onSwiper={(swiper) => setSwiperRef(swiper)}
+                                                    slidesPerView={1}
+                                                    spaceBetween={30}
+                                                    centeredSlides={false}
+                                                    loop
+                                                    modules={[]}
+                                                    className="mySwiper"
+                                                    autoplay={{
+                                                        delay: 2500,
+                                                        disableOnInteraction: false
+                                                    }}
+                                                >
+                                                    {images.map((item: any, index) => (
+                                                        <SwiperSlide key={index}>
+                                                            <img src={item.url} />
+                                                        </SwiperSlide>
+                                                    ))}
+                                                </Swiper>
+                                            )}
+                                        </div>
+                                    </>
+                                )}
+                            </div>
+                        </Spin>
+                        <Spin tip="Loading..." spinning={textLoading}>
+                            <div className="col-span-1">
+                                {images.length > 0 && (
+                                    <div className="w-full h-full p-4">
+                                        <div className="flex items-center justify-between">
+                                            <div className="flex items-center">
+                                                <Avatar />
+                                                <span className="text-[rgba(51,51,51,0.8)] text-base ml-2">沾不沾果酱</span>
+                                            </div>
+                                            <div
+                                                className="bg-[#ff2e4d] text-white text-base w-[96px] font-semibold px-6 h-[40px] cursor-pointer rounded-2xl text-center"
+                                                style={{ lineHeight: '40px' }}
                                             >
-                                                {images.map((item: any, index) => (
-                                                    <SwiperSlide key={index}>
-                                                        <img src={item.url} />
-                                                    </SwiperSlide>
-                                                ))}
-                                            </Swiper>
-                                        )}
-                                    </div>
-                                </>
-                            )}
-                        </div>
-                        <div className="col-span-1">
-                            {images.length > 0 && (
-                                <div className="w-full h-full p-4">
-                                    <div className="flex items-center justify-between">
-                                        <div className="flex items-center">
-                                            <Avatar />
-                                            <span className="text-[rgba(51,51,51,0.8)] text-base ml-2">沾不沾果酱</span>
+                                                关注
+                                            </div>
                                         </div>
-                                        <div
-                                            className="bg-[#ff2e4d] text-white text-base w-[96px] font-semibold px-6 h-[40px] cursor-pointer rounded-2xl text-center"
-                                            style={{ lineHeight: '40px' }}
-                                        >
-                                            关注
-                                        </div>
+                                        <div className="font-semibold text-lg mb-2 mt-8">{text}</div>
+                                        <Divider />
                                     </div>
-                                    <div className="font-semibold text-lg mb-2 mt-8">{text}</div>
-                                    <Divider />
-                                </div>
-                            )}
-                        </div>
+                                )}
+                            </div>
+                        </Spin>
                     </div>
                 </Card>
             </Space>

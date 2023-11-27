12e25376d017d8697665f7af5d6954d7294b18b3
Walker Peng <939212596@qq.com>
2023-11-27 18:47:09 +0800

    feat: update

--- src/views/pages/thesaurusDetail/component/AddKeywordDrawer.tsx
+++ src/views/pages/thesaurusDetail/component/AddKeywordDrawer.tsx
@@ -58,13 +58,25 @@ export const AddKeywordDrawer = ({ open, handleClose, uid, forceUpdate }: IAddKe
                         rows={11}
                         label={'关键词'}
                         InputLabelProps={{ shrink: true }}
+                        value={keyWord}
                         placeholder={`一行一组关键词，如：
 flashlight
 led flashlight
 mini led flashlight`}
                         className="w-full"
                         onChange={(e) => {
-                            setKeyWord(e.target.value);
+                            const text = e.target.value;
+                            // 匹配开头和结尾的标点符号
+                            const regex = /^[^\w\s]+|[^\w\s]+$/g;
+
+                            // 将文本分成多行
+                            const lines = text.split('\n');
+
+                            // 去掉每行开头和结尾的标点符号，并确保单词之间只有一个空格
+                            const cleanedLines = lines.map((line) => line.replace(regex, '').replace(/\s+/g, ' '));
+
+                            const cleanedText = cleanedLines.join('\n');
+                            setKeyWord(cleanedText);
                         }}
                     />
                 </div>

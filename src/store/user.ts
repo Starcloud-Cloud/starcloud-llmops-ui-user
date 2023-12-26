71c6bfad9c130455ddfd2ec00fa4c944dcceec1a
Walker Peng <939212596@qq.com>
2023-12-26 19:40:21 +0800

    feat: update

--- src/store/user.ts
+++ src/store/user.ts
@@ -57,8 +57,8 @@ const useUserStore = create<UserStore>((set) => ({
             user: userInfo.user,
             isSetUser: true
         });
-        wsCache.set(CACHE_KEY.USER, userInfo);
-        // wsCache.set(CACHE_KEY.ROLE_ROUTERS, userInfo?.menus);
+        // wsCache.set(CACHE_KEY.USER, userInfo);
+        wsCache.set(CACHE_KEY.ROLE_ROUTERS, userInfo?.menus);
     },
     loginOut: async () => {
         await loginOut();

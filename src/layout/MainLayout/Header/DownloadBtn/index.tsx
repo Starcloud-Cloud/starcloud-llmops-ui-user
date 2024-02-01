6dd6aaf2338a412778cf30721a26b3f53a979a85
Walker Peng <939212596@qq.com>
2024-02-01 15:09:55 +0800

    feat: update

--- src/layout/MainLayout/Header/DownloadBtn/index.tsx
+++ src/layout/MainLayout/Header/DownloadBtn/index.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { isMobile } from 'react-device-detect';
 import { useAllDetail } from 'contexts/JWTContext';
 import { useEffect, useState } from 'react';
+import { InviteUser } from 'ui-component/invite-user';
 
 export const DownLoadBtn = () => {
     const install = usePWAInstall();
@@ -18,19 +19,32 @@ export const DownLoadBtn = () => {
 export const PayBtn = () => {
     const navigate = useNavigate();
     const allDetail = useAllDetail();
-    const [text, setText] = useState('年末5折限时体验');
+    const [text, setText] = useState('9.9老用户专享');
+    const [open, setOpen] = useState(false);
+
     useEffect(() => {
         if (allDetail?.allDetail?.isNewUser) {
             setText('新用户限时体验9.9元');
         }
     }, []);
 
+    const handleClick = () => {
+        if (allDetail?.allDetail?.isNewUser) {
+            navigate('/subscribe');
+        } else {
+            setOpen(true);
+        }
+    };
+
     return !isMobile ? (
-        <button className="btn-grad flex items-center" onClick={() => navigate('/subscribe')}>
-            <span>{text}</span>
-            <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1614" width="16" height="16">
-                <path d="M377.9 114.1h358.3l-136.4 268 231.5 0.3-485.1 559.3 139.5-356.4H222.6z" fill="#FED928" p-id="1615"></path>
-            </svg>
-        </button>
+        <>
+            <button className="btn-grad flex items-center" onClick={() => handleClick()}>
+                <span>{text}</span>
+                <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1614" width="16" height="16">
+                    <path d="M377.9 114.1h358.3l-136.4 268 231.5 0.3-485.1 559.3 139.5-356.4H222.6z" fill="#FED928" p-id="1615"></path>
+                </svg>
+            </button>
+            {open && <InviteUser onClose={() => setOpen(false)} />}
+        </>
     ) : null;
 };

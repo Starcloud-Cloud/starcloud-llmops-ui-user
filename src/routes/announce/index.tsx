92ab605ecaadf70ba9b63437669f2cd9741c8f3e
Walker Peng <939212596@qq.com>
2024-02-20 14:21:19 +0800

    feat: update

--- src/routes/announce/index.tsx
+++ src/routes/announce/index.tsx
@@ -11,6 +11,8 @@ import AuthenticationRoutes from './AuthenticationRoutes';
 const PageNotFound = Loadable(lazy(() => import('views/pages/maintenance/Error')));
 const PagesLanding = Loadable(lazy(() => import('views/announce/landing')));
 const PagesPrice = Loadable(lazy(() => import('views/announce/pricing/member')));
+const AppUserAccountProfile = Loadable(lazy(() => import('views/application/users/account-profile/Profile')));
+const OrderRecord = Loadable(lazy(() => import('views/pages/pricing/orderRecord')));
 
 // ==============================|| ROUTING RENDER ||============================== //
 
@@ -19,6 +21,8 @@ export default function ThemeRoutes() {
     return useRoutes([
         { path: '/', element: <PagesLanding /> },
         { path: '/subscribe', element: <PagesPrice /> },
+        { path: '/web-view/orderRecord', element: <OrderRecord /> },
+        { path: '/web-view/account-profile', element: <AppUserAccountProfile /> },
         { path: '/admin-api/*', element: null },
         { path: '/app-api/*', element: null },
         LoginRoutes,

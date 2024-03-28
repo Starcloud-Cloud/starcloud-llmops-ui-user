79e3fd6b9e052e7b4a549593826a6bbf671bcadb
Walker Peng <939212596@qq.com>
2024-03-28 18:51:07 +0800

    feat: update

--- src/views/pages/copywriting/components/spliceCmponents/exePrompt.tsx
+++ src/views/pages/copywriting/components/spliceCmponents/exePrompt.tsx
@@ -41,20 +41,20 @@ const ExePrompt = ({ type, changePrompt, flag }: { type: string; changePrompt: (
             // open={open}
             trigger={'click'}
             content={
-                <div>
+                <div className="w-[640px]">
                     <div className="flex border-b border-solid border-red-50">
                         <Input placeholder="请输入名称" onChange={(e: any) => setValue(e.target.value)} />
                     </div>
                     <Divider style={{ margin: '6px 0' }} />
-                    <div className="flex flex-wrap w-[350px]">
+                    <div className="flex flex-wrap w-full">
                         {tags.flat().map((item: any) => (
                             <Tag className="cursor-pointer mb-1" onClick={() => handleTag(item)}>
                                 {item}
                             </Tag>
                         ))}
                     </div>
-                    <div className="w-[350px] flex gap-2 pt-2 h-[250px] overflow-y-auto">
-                        <div className="w-[100px]">
+                    <div className="w-full flex gap-2 pt-2 h-[300px] overflow-y-auto">
+                        <div className="w-[30%]">
                             {dictList?.map((item) => (
                                 <div
                                     className={` ${
@@ -75,7 +75,7 @@ const ExePrompt = ({ type, changePrompt, flag }: { type: string; changePrompt: (
                                 </div>
                             ))}
                         </div>
-                        <div className="w-[250px] text-sm bg-[#fafafa] rounded p-1 overflow-y-auto line-clamp-1">{currentDict.remark}</div>
+                        <div className="text-sm flex-1 bg-[#fafafa] rounded p-1 overflow-y-auto line-clamp-1">{currentDict.remark}</div>
                     </div>
                 </div>
             }

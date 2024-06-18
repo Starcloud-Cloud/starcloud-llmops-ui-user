6c9e7e5062bd74c504e80c5baca30711464928e0
shenqichao <2310776744@qq.com>
2024-06-18 18:13:42 +0800

    update

--- src/views/pages/smallRedBook/components/form.tsx
+++ src/views/pages/smallRedBook/components/form.tsx
@@ -1,11 +1,48 @@
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import { useState } from 'react';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { Upload } from 'antd';
+import type { UploadProps } from 'antd';
+import { getAccessToken } from 'utils/auth';
 const Form = ({ item, index, changeValue, flag }: { item: any; index: number; changeValue: any; flag?: boolean }) => {
     const mt = {
         marginTop: 2
     };
     const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const uploadButton = (
+        <button style={{ border: 0, background: 'none', marginTop: 16 }} type="button">
+            {loading ? <LoadingOutlined rev={undefined} /> : <PlusOutlined rev={undefined} />}
+            <div style={{ marginTop: 8 }}>Upload</div>
+        </button>
+    );
+    const props: UploadProps = {
+        name: 'image',
+        multiple: true,
+        listType: 'picture-card',
+        showUploadList: false,
+        action: `${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_API_URL}/llm/creative/plan/uploadImage`,
+        headers: {
+            Authorization: 'Bearer ' + getAccessToken()
+        },
+        maxCount: 1,
+        onChange(info) {
+            console.log(info);
+
+            if (info.file.status === 'uploading' && loading === false) {
+                setLoading(true);
+                return;
+            }
+            if (info.file.status === 'done') {
+                setLoading(false);
+                changeValue({ index, value: info?.file?.response?.data?.url });
+            }
+        },
+        onDrop(e) {
+            console.log('Dropped files', e.dataTransfer.files);
+        }
+    };
     return (
         <div>
             {item.style === 'INPUT' ? (
@@ -80,6 +117,10 @@ const Form = ({ item, index, changeValue, flag }: { item: any; index: number; ch
                         </MenuItem>
                     ))}
                 </TextField>
+            ) : item.style === 'IMAGE' ? (
+                <Upload {...props}>
+                    {item.value ? <img className="mt-4" src={item.value} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+                </Upload>
             ) : null}
         </div>
     );

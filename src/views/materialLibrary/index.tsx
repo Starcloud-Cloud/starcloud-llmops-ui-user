031588f0634ecaee623c6f0bf6384dc4572c2744
Walker Peng <939212596@qq.com>
2024-07-03 15:55:37 +0800

    feat: update

--- src/views/materialLibrary/index.tsx
+++ src/views/materialLibrary/index.tsx
@@ -1,8 +1,16 @@
-import { Select, Input, Row, Col, Tabs, Table, Space, Button } from 'antd';
+import { Select, Input, Row, Col, Tabs, Table, Space, Button, Modal, Form, Switch, Avatar } from 'antd';
 import type { TabsProps, TableProps } from 'antd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import IconSelect from 'ui-component/IconSelect';
+import Icon, { ApiOutlined, UploadOutlined } from '@ant-design/icons';
+import { CheckCard } from '@ant-design/pro-components';
+const { Option } = Select;
+
 const MaterialLibrary = () => {
-    const { Option } = Select;
+    const [tableData, setTableData] = useState<any[]>([]);
+    const [isModalOpen, setIsModalOpen] = useState(true);
+    const [selectIcon, setSelectIcon] = useState('');
+
     const columns: TableProps<any>['columns'] = [
         {
             title: '名称',
@@ -38,7 +46,7 @@ const MaterialLibrary = () => {
             )
         }
     ];
-    const [tableData, setTableData] = useState<any[]>([{}]);
+
     const TabsItems: TabsProps['items'] = [
         {
             label: '我的素材库',
@@ -51,6 +59,21 @@ const MaterialLibrary = () => {
         },
         { label: '系统素材库', key: '2', children: <div>222</div> }
     ];
+
+    useEffect(() => {}, []);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div className="bg-white h-full p-4">
             <Row gutter={[16, 16]}>
@@ -64,6 +87,62 @@ const MaterialLibrary = () => {
                 </Col>
             </Row>
             <Tabs items={TabsItems}></Tabs>
+            <Modal width={580} title="创建知识库" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                <Form layout="vertical">
+                    <Form.Item label="名称" name="disabled" rules={[{ required: true }]}>
+                        <Input placeholder="填写名称" />
+                    </Form.Item>
+                    <Form.Item label="描述" rules={[{ required: true }]}>
+                        <Input.TextArea placeholder="填写描述" />
+                    </Form.Item>
+                    <Form.Item label="分类" rules={[{ required: true }]}>
+                        <Select>
+                            <Option value={1}>123</Option>
+                            <Option value={2}>123</Option>
+                        </Select>
+                    </Form.Item>
+
+                    <Icon component={selectIcon as any} />
+                    <Form.Item label="图标" rules={[{ required: true }]}>
+                        <IconSelect
+                            onChange={(value) => {
+                                setSelectIcon(value);
+                            }}
+                            value={selectIcon}
+                        />
+                    </Form.Item>
+                    <Form.Item name="checkbox-group" className="w-full" label="导入类型">
+                        <CheckCard.Group style={{ width: '100%' }}>
+                            <Row gutter={16}>
+                                <Col span={12}>
+                                    <CheckCard
+                                        className="w-[100%]"
+                                        title="本地文档"
+                                        avatar={<UploadOutlined />}
+                                        description="上传Excel或者CSV格式的文档"
+                                        value="SpringBoot"
+                                    />
+                                </Col>
+                                <Col span={12}>
+                                    <CheckCard
+                                        className="w-[100%]"
+                                        title="API"
+                                        avatar={<ApiOutlined />}
+                                        description="获取JSON格式API内容"
+                                        value="SOFABoot"
+                                    />
+                                </Col>
+                            </Row>
+                        </CheckCard.Group>
+                    </Form.Item>
+                    <Form.Item label="是否系统库" rules={[{ required: true }]}>
+                        <Switch />
+                    </Form.Item>
+                    <Form.Item label="是否分享给团队" rules={[{ required: true }]}>
+                        <Switch />
+                    </Form.Item>
+                </Form>
+            </Modal>
         </div>
     );
 };

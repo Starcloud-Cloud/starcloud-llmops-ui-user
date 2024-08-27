2b9850fdc05af3a0a7deaf1db26c2e3cc95c55ea
shenqichao <2310776744@qq.com>
2024-08-27 14:28:27 +0800

    updata

--- src/ui-component/Markdown/editMarkdown.tsx
+++
@@ -1,5 +0,0 @@
-import Editor from '@monaco-editor/react';
-const EditMarkDown = () => {
-    return <Editor height="400px" defaultLanguage="javascript" defaultValue="// some comment" />;
-};
-export default EditMarkDown;
